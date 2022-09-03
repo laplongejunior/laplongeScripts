e17a556ed80dddcdff3d5ea017d9646731368d7e
laplongejunior <thierry.ladeuze@yahoo.fr>
2022-09-03 15:05:26 +0200

    Added error handler for new version

--- metroDownloader/metro.bat
+++ metroDownloader/metro.bat
@@ -1,2 +1,15 @@
-java -jar "%~dp0MetroDownloader-0.0.1-SNAPSHOT.jar" "CHROME" "%~dp0drivers\chromedriver.exe" "%~dp0pdfs"
+java -jar "%~dp0MetroDownloader-0.0.2-SNAPSHOT.jar" "CHROME" "%~dp0drivers\chromedriver.exe" "%~dp0pdfs"
+SET "error=%ERRORLEVEL%"
+@ECHO OFF
+IF %error% EQU 0 (
+	ECHO "Download completed"
+) ELSE IF %error% EQU 1 (
+	ECHO "Unexpected error, check stacktrace"
+) ELSE IF %error% EQU 2 (
+	ECHO "Missing argument"
+) ELSE IF %error% EQU 3 (
+	ECHO "Download location doesn't exist"
+) ELSE (
+	ECHO "Undocumented error code: %error%"
+)
 PAUSE
