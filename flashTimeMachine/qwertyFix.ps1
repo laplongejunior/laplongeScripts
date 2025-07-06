4f5d0217b186f5164dab5a22c87e30c56012b999
laplongejunior <thierry.ladeuze@yahoo.fr>
2025-07-06 23:33:33 +0200

    Create qwertyFix.ps1

---
+++ flashTimeMachine/qwertyFix.ps1
@@ -0,0 +1,15 @@
+$flag=Test-Path $PSScriptRoot"\qwertyFix.txt"
+#$flag=1
+
+$lang = Get-WinUserLanguageList
+#$lang="fr-BE"
+
+if ($flag) {
+	$temp = Get-WinUserLanguageList
+	$temp.Insert(0, "en-US")
+	Set-WinUserLanguageList -LanguageList $temp -Force
+}
+Start-Process $PSScriptRoot"\DA-hardlink.exe" -Wait
+if ($flag) {
+	Set-WinUserLanguageList -LanguageList $lang -Force
+}
