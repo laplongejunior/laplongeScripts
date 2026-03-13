456fd801b095e2e2afa6d7a6ea4a11454aa490aa
laplongejunior <thierry.ladeuze@yahoo.fr>
2026-03-13 14:18:54 +0100

    Added support when joining from front page
    This *massive* updates changes a lot to fix a minor issue : when we go from the homepage to a creator, the campaign object isn't loaded in the same way, and instead there's only partial references to the campaigns.  
    By adding manual fetching of Patreon's API rather than merely grabbing the already-data, it's now possible to deduce it from the campaign's ID, loaded even on the front page
    
    Practical changes : 
    1) Added on top of the script a flag to disable fetches calls added by the new feature
    2) If the currency is not found the regular way, the latest_campaigns is also processed. If it doesn't have currency data (as of now it never does) it will provide the name->id mapping we need to call Patreon's API
    
    Technical changes : 
    3) The processing in FIND_CURRENCY now uses a submethod _campaignToCurrency, to allow to do the conversion from either one campaign as before, or the now more complex processing of latest_campaigns  
    4) FIND_CURRENCY itself is now tagged as async, as it could have to wait fetch. The structure of the method insertCurrency as been modified to deal with a promise rather than a direct return value
    5) CURRENCY_CACHE now stores and checks for explicit nulls for the specific case of "currency is unknown, but don't start a new manual fetch anyway"

--- patreonCurrency/userscript.js
+++ patreonCurrency/userscript.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Patreon currency finder
-// @version      0.0.1
+// @namespace    http://tampermonkey.net/
+// @version      0.0.2
 // @description  Adds creator's currency on patreon campaign pages
 // @author       laplongejunior
 // @license      https://www.gnu.org/licenses/agpl-3.0.fr.html
 // @match        https://www.patreon.com/*
+// @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
@@ -13,6 +15,9 @@
     // Use document-start in order to guarantee an early hookup to onload
     // Else, the script will visually work by executing at document-end, but React will throw minified errors due to modifying the DOM before the page finished loaded
     global.addEventListener('load',()=>{
+        // If TRUE, if the script identifies a campaign object without the currency data, it will call Patreon's API to load the full campaign to identify the currency
+        // As the currency is put in cache, a specific campaign will only be fetched once per page load
+        const ALLOW_FETCH = true;
 
         // Patreon regularily changes the way they store the general data
         // Feel free to do a console.log(window) to see all the global space
@@ -28,34 +33,82 @@
         // Interesting funfact about Patreon : they *don't* reload the whole page when changing navigation
         // So the "currency" changes if we jump from a creator to another
         const CURRENCY_CACHE = {};
-        const FIND_CURRENCY = (name)=>{
+        const FIND_CURRENCY = async (name)=>{
             let result = CURRENCY_CACHE[name];
             if (result) return result;
+
+            // Prevents multiple lookups while we are waiting for a network request
+            // All other methods use "falsy" values, so here we specifically use null as a guard value for "manual loading in progress or failed, do not retry requests"
+            const fetchAllowed = (ALLOW_FETCH && result !== null);
             result = data.campaign;
+
+            // Finds the currency from a campaign's data object
+            // If not found, provides the campaign's name to identify which one is needed *now*
+            const _campaignToCurrency = (result)=>{
+                result = result.attributes;
+                if (!result) return;
+                const key = result.name;
+                result = result.currency;
+                if (result) {
+                    CURRENCY_CACHE[key] = result;
+                    console.log("Detected currency: "+result);
+                }
+                else console.log("Couldn't detect creator's currency");
+                return { key , result };
+            };
+
             if (result) result = result.data;
-            if (result) result = result.attributes;
-            if (result) result = result.currency;
-            CURRENCY_CACHE[name] = result;
-            console.log("Detected currency: "+result);
-            return result;
+            if (result) result = _campaignToCurrency(result);
+            if (result) return result.result;
+
+            // When coming from home page, there's not *one* campaign objects but several references to various campaigns
+            result = data.latest_campaigns;
+            if (result) result = result.data;
+            if (!result) return;
+
+            for (let item of result) {
+                result = _campaignToCurrency(item);
+                if (!result) continue;
+
+                const key = result.key;
+                result = result.result;
+                if (result || !key) continue; // result is never truthty in practice, as the API doesn't return the currency code in those smaller objects :(
+
+                result = item.id;
+                // An indirect lookup may be too expensive for all campaigns at once
+                if (!result || !fetchAllowed || key !== name) continue;
+                CURRENCY_CACHE[name] = null; // Prevents including fetches in a possible loop (see the guard check on top of the method)
+
+                result = await fetch("https://www.patreon.com/api/campaigns/"+result);
+                if (result && result.ok) result = await result.json();
+                if (result) result = result.data;
+                if (result) result =_campaignToCurrency(result);
+                if (result) result = result.result;
+                return result;
+            };
         };
 
         const insertCurrency=(parent)=>{
             // Prevents an infinite loop
             if (parent.childNodes.length >= 2) return;
 
-            const container = global.document.createElement('div');
-            console.log(parent.textContent);
-            container.appendChild(document.createTextNode(FIND_CURRENCY(parent.textContent)));
+            FIND_CURRENCY(parent.textContent).then(currency=>{
+                // In case insertions waited for fetches to happen
+                if (!currency || parent.childNodes.length >= 2) return;
+                console.log(currency);
 
-            // We can reuse a few classes used by Patreon
-            container.classList.add('cm-dMgEsi');
-            container.classList.add('cm-LNraKM');
-            container.classList.add('cm-DFAJDB');
-            container.classList.add('cm-dupTbP');
+                const container = global.document.createElement('div');
+                console.log(parent.textContent);
+                container.appendChild(document.createTextNode(currency));
 
-            parent.appendChild(container);
-            return container;
+                // We can reuse a few classes used by Patreon
+                container.classList.add('cm-dMgEsi');
+                container.classList.add('cm-LNraKM');
+                container.classList.add('cm-DFAJDB');
+                container.classList.add('cm-dupTbP');
+
+                parent.appendChild(container);
+            });
         };
 
         const safeQuerySelector = (element, query)=>{
@@ -78,7 +131,7 @@
             for (const tag of ['upgrade-free-membership-button', 'become-patron-button'])
                 for (title of target.querySelectorAll('a[data-tag="'+tag+'"]')) {
                     title = safeQuerySelector(title.parentNode,'a p');
-                    if (title) console.log(insertCurrency(title));
+                    if (title) insertCurrency(title);
                 }
         };
 
